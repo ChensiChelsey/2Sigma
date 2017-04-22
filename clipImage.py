d5f5f131d8585575170a506dc89f40275e2a12c2
Dragoncell <jmxuzju@qq.com>
2017-04-21 22:26:14 -0400

    delete im2

--- clipImage.py
+++ clipImage.py
@@ -7,7 +7,7 @@ import scipy.misc
 im = cv2.imread("./test_1.png")
 imgrey = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(imgrey, 127, 255, 0)
-im2, contours, hierachy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+contours, hierachy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 for cnt in contours:
     x,y,w,h = cv2.boundingRect(cnt)
