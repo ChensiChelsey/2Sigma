930c1dd7e47ed8e936073b1802d9016f437e7ba4
chensi <chensi@chensis-MBP.fios-router.home>
2017-04-23 00:00:08 -0400

    Modifying the function for bounding box around element in expression. Add the function of bounding external box.

--- clipImage.py
+++ clipImage.py
@@ -1,39 +1,46 @@
-from PIL import Image, ImageDraw
 import PIL.ImageOps
 import numpy as np
 import cv2
 import scipy.misc
 from scipy.misc import imsave
+from PIL import Image, ImageDraw
 
-# clipt image into box using OpenCV
-# i = Image.open("./test_T.png")
-# i = PIL.ImageOps.invert(i)
-# print type(i)
+input = "eq29" #set the name of image
+im = cv2.imread("./test_image/" + input + ".png")
 
-# image_file = Image.open("test_T.png") # open colour image
-# # image_file= image_file.convert('L') # convert image to monochrome - this works
-# image_file= image_file.convert('1') # convert image to black and white
-# imsave('test_T_new.png', image_file)
+# convert the image into white and black image
+im[im >= 127] = 255
+im[im < 127] = 0
 
-im = cv2.imread("./test_T.png")
-kernel = np.ones((5,5),np.uint8)
+# set the morphology kernel size, the number in tuple is the bold pixel size
+kernel = np.ones((3,3),np.uint8)
 im = cv2.morphologyEx(im, cv2.MORPH_CLOSE, kernel)
-# im = cv2.bitwise_not(im_inv)
+
+# make a tmpelate image for next crop
 image = Image.fromarray(im)
 image.save("temp.png")
+
+# create grey image for retrieving contours
 imgrey = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(imgrey, 127, 255, 0)
-im2, contours, hierachy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+im2, contours, hierachy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # RETR_EXTERNAL for only bounding outer box
 
+# bounding rectangle outside the individual element in image
 num = 1
 for cnt in contours:
     x,y,w,h = cv2.boundingRect(cnt)
+    # exclude the whole size image
     if x is 0: continue
+    # save rectangled element
     symbolImage = image.crop((x, y, x+w, y+h))
-    symbolImage.save(str(num) + ".png")
+    symbolImage.save("./image/" + input + "_" + str(num) + "_" + str(y) + "_" + str(y+h) + "_" + str(x) + "_" + str(x+w) + ".png")
+    # fill the found part with black to reduce effect to other crop
     draw = ImageDraw.Draw(image)
-    draw.rectangle((x, y, x+w, y+h), fill = 'white')
+    draw.rectangle((x, y, x+w, y+h), fill = 'black')
+    # draw rectangle around element in image for confirming result
     cv2.rectangle(im, (x,y), (x+w, y+h), (0,255,0), 2)
     num = num + 1
+    
+# save bouding result
 image = Image.fromarray(im)    
-image.save("test.png")
+image.save("boudingResult" + input + ".png")
